cc4d5c4e9e24bcf1f103fd95bbd8d33ff0f8e10b
Nicolas1503 <nicolas.sforza@gmail.com>
2024-06-30 22:11:23 +0000

    CAMBIO A LED BLUEPILL

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ int main()
 {
     DigitalIn B1_USER(BUTTON1);
 
-    DigitalOut LD2(LED1);
+    DigitalOut LD13(LED1);
 
     while (true) {
-        LD2 = B1_USER;
+        LD13 = B1_USER;
     }
 }
