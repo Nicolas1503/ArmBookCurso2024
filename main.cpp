8091005c1f79bcd9cbfb45ec0259cfb8a8cef546
Nicolas1503 <nicolas.sforza@gmail.com>
2024-07-01 21:46:18 +0000

    PRUEBA CON PINES BLUEPILL

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "mbed.h"
 
+#define LED1 PA_1
+#define BUTTON1 PA_2
+
+
 int main()
 {
-    DigitalIn B1_USER(BUTTON1);
+    //DigitalIn B1_USER(BUTTON1);
 
-    DigitalOut LD13(LED1);
+    DigitalOut LD1(LED1);
+    
 
-    while (true) {
-        LD13 = B1_USER;
+    /*while (true) {
+        LD1 = B1_USER;
     }
+    */
 }
